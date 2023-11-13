223850ed3a99c8d4808e84c201fa44b0d783785d
xinxiao <xinxiao0214@outlook.com>
2023-11-14 00:19:05 +0800

    实现Vector2基本接口

--- src/math/Vector2.ts
+++ src/math/Vector2.ts
@@ -12,10 +12,136 @@ class Vector2 extends BaseObject implements ICloneable {
      * @param x x component
      * @param y y component
      */
-    constructor(x: number=0.0, y: number=0.0) {
+    constructor(x: number = 0.0, y: number = 0.0) {
         super();
         this.x = x;
         this.y = y;
+    }
+
+    /**
+     * adds the given Vector2 object to the current Vector2 object
+     * @param vec2 the other Vector2 object
+     * @returns the current updated object
+     */
+    add(vec2: this): this {
+        this.x += vec2.x;
+        this.y += vec2.y;
+        return this;
+    }
+
+    /**
+     * subtracts the given Vector2 object from the current Vector2 object
+     * @param vec2 the other Vector2 object
+     * @returns the current updated object
+     */
+    subtract(vec2: this): this {
+        this.x -= vec2.x;
+        this.y -= vec2.y;
+        return this;
+    }
+
+    /**
+     * multiplies the current Vector2 object by the given Vector2 object
+     * @param vec2 the other Vector2 object
+     * @returns the current updated Vector2 object
+     */
+    multiply(vec2: this): this {
+        this.x *= vec2.x;
+        this.y *= vec2.y;
+        return this;
+    }
+
+    /**
+     * divides the current Vector2 object by the given Vector2 object
+     * @param vec2 the other Vector2 object
+     * @returns the current updated object
+     */
+    divide(vec2: this): this {
+        if (vec2.x === 0 || vec2.y === 0) {
+            throw new Error("divide by zero");
+        }
+
+        this.x /= vec2.x;
+        this.y /= vec2.y;
+        return this;
+    }
+
+    /**
+     * nagate the current Vector2
+     * @returns the current updated object
+     */
+    negate(): this {
+        this.x = -this.x;
+        this.y = -this.y;
+        return this;
+    }
+
+    /**
+     * multiplies the current Vector2 object by given scale
+     * @param scale the scale factor
+     * @returns the current updated object
+     */
+    scale(scale: number): this {
+        this.x *= scale;
+        this.y *= scale;
+        return this;
+    }
+
+    /**
+     * gets the length of the vector
+     * @returns the length of the vector
+     */
+    length(): number {
+        return Math.sqrt(this.squaredLength());
+    }
+
+    /**
+     * gets the squared length of the vector
+     * @returns the squared length of the vector
+     */
+    squaredLength(): number {
+        return this.x * this.x + this.y * this.y;
+    }
+
+    /**
+     * gets the distance between the current Vector2 object and the given Vector2 object
+     * @param vec2 the other Vector2 object
+     * @returns the distance
+     */
+    distanceTo(vec2: Vector2): number {
+        return Vector2.Distance(this, vec2);
+    }
+
+    /**
+     * gets the squared distance between the current Vector2 object and the given Vector2 object
+     * @param vec2 the other Vector2 object
+     * @returns the squared distance
+     */
+    squaredDistanceTo(vec2: Vector2): number {
+        return Vector2.SquaredDistance(this, vec2);
+    }
+
+    /**
+     * get the dot product of the current Vector2 object and the given Vector2 object
+     * @param vec2 the other Vector2 object
+     * @returns the dot product
+     */
+    dot(vec2: Vector2): number {
+        return Vector2.Dot(this, vec2);
+    }
+
+    /**
+     * normalize the vector
+     * @returns the current object
+     */
+    normalize(): this {
+        const length = this.length();
+        if (length === 0) {
+            return this;
+        }
+        this.x /= length;
+        this.y /= length;
+        return this;
     }
 
     /**
@@ -51,6 +177,37 @@ class Vector2 extends BaseObject implements ICloneable {
      */
     static Equals(lhs: Vector2, rhs: Vector2): boolean {
         return lhs.x === rhs.x && lhs.y === rhs.y;
+    }
+
+    /**
+     * gets the distance between the "value1" vector and the "value2" vector
+     * @param value1 the first Vector2 object
+     * @param value2 the second Vector2 object
+     * @returns the distance
+     */
+    static Distance(value1: Vector2, value2: Vector2): number {
+        return Math.sqrt(this.SquaredDistance(value1, value2));
+    }
+
+    /**
+     * gets the squared distance between the "value1" vector and the "value2" vector
+     * @param value1 the first Vector2 object
+     * @param value2 the second Vector2 object
+     * @returns the squared distance
+     */
+    static SquaredDistance(value1: Vector2, value2: Vector2): number {
+        const sub = value1.clone().subtract(value2);
+        return sub.squaredLength();
+    }
+
+    /**
+     * gets the dot product of the "value1" vector and the "value2" vector
+     * @param value1 the first Vector2 object
+     * @param value2 the second Vector2 object
+     * @returns the dot product
+     */
+    static Dot(value1: Vector2, value2: Vector2): number {
+        return value1.x * value2.x + value1.y * value2.y;
     }
 
     /**
--- test/math/Vector2.test.ts
+++ test/math/Vector2.test.ts
@@ -19,6 +19,67 @@ describe("teset Vector2", () => {
         expect(vec2.y).toBe(1.0)
     })
 
+    test("add", () => {
+        const vec2 = new Vector2(1.0, 1.0);
+        expect(new Vector2(1,2).add(vec2)).toEqual(new Vector2(2.0,3.0));
+    })
+
+    test("subtract", () => {
+        const vec2 = new Vector2(1.0, 1.0);
+        expect(new Vector2(1,2).subtract(vec2)).toEqual(new Vector2(0.0,1.0));
+    })
+
+    test("multiply", () => {
+        const vec2 = new Vector2(2.0, 2.0);
+        expect(new Vector2(1,2).multiply(vec2)).toEqual(new Vector2(2.0,4.0));
+    })
+
+    test("divide", () => {
+        const vec2 = new Vector2(2.0, 2.0);
+        expect(new Vector2(1,2).divide(vec2)).toEqual(new Vector2(0.5,1.0));
+        expect(()=>new Vector2(1,2).divide(new Vector2(0.0,0.0))).toThrowError();
+    })
+
+    test("negate", () => {
+        const vec2 = new Vector2(2.0, 2.0);
+        expect(vec2.negate()).toEqual(new Vector2(-2.0,-2.0));
+    })
+
+    test("scale", () => {
+        const vec2 = new Vector2(2.0, 2.0);
+        expect(vec2.scale(2)).toEqual(new Vector2(4.0,4.0));
+    })
+
+    test("normalize", () => {
+        const vec2 = new Vector2(2.0, 0.0);
+        expect(vec2.normalize()).toEqual(new Vector2(1.0,0.0));
+    })
+
+    test("dot", () => {
+        const vec2 = new Vector2(2.0, 0.0);
+        expect(vec2.dot(new Vector2(1.0,0.0))).toEqual(2.0);
+    })
+
+    test("length", () => {
+        const vec2 = new Vector2(2.0, 2.0);
+        expect(vec2.length()).toEqual(Math.sqrt(8.0));
+    })
+
+    test("lengthSquared", () => {
+        const vec2 = new Vector2(2.0, 2.0);
+        expect(vec2.squaredLength()).toEqual(8.0);
+    })
+
+    test("distanceTo", () => {
+        const vec2 = new Vector2(2.0, 2.0);
+        expect(vec2.distanceTo(new Vector2(2.0,0.0))).toEqual(2.0);
+    })
+
+    test("squaredDistanceTo", () => {
+        const vec2 = new Vector2(2.0, 2.0);
+        expect(vec2.squaredDistanceTo(new Vector2(2.0,0.0))).toEqual(4.0);
+    })
+
     test("toString", () => {
         const vec2 = new Vector2(1, 1);
         expect(vec2.toString()).toEqual("{x:1,y:1}");
